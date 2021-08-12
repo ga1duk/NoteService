388451dfd6b08a0baeaa92c400bdef04f8fb4d95
Dmitry Gaiduk <gaidukdg@yandex.ru>
2021-08-12 22:24:09 +0300

    add() function was modified; delete() function was realized

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,28 @@ fun main() {
 
     val note1 = Note(title = "Do or die", text = "Some interesting text")
     val note2 = Note(title = "Do or die2", text = "Some interesting text2")
+    val note3 = Note(title = "Do or die3", text = "Some interesting text3")
 
     val noteService = NoteService()
 
     noteService.add(note1)
     noteService.add(note2)
 
-    val iterator = noteService.notes.iterator()
-    while (iterator.hasNext()) {
-        println(iterator.next())
+    for (note in noteService.notes) {
+        println(note)
+    }
+
+    noteService.delete(1)
+
+    println()
+    for (note in noteService.notes) {
+        println(note)
+    }
+
+    noteService.add(note3)
+
+    println()
+    for (note in noteService.notes) {
+        println(note)
     }
 }
--- src/main/kotlin/NoteService.kt
+++ src/main/kotlin/NoteService.kt
@@ -2,18 +2,27 @@ class NoteService {
 
     val notes = mutableListOf<Note>()
 
-    fun add(note: Note): Note {
-        note.id = notes.size + 1
+    fun add(note: Note) {
+//        Если список пустой, присвоить заметке id = 1, если список не пустой,
+//        присвоить заметке id на 1 больше, чем id у последней заметки в списке
+        if (notes.isEmpty()) {
+            note.id = 1
+        } else {
+            note.id = notes.last().id + 1
+        }
         notes.add(note)
-        return notes.last()
     }
 
     fun createComment() {
 
     }
 
-    fun delete() {
-
+    fun delete(id: Int) {
+        for (note in notes) {
+            if (id == note.id) {
+                notes.remove(note)
+            }
+        }
     }
 
     fun deleteComment() {
