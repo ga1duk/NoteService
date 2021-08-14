7510257c4cbb7ca169e01ec905ee4aa7d92326d1
Dmitry Gaiduk <gaidukdg@yandex.ru>
2021-08-14 14:56:30 +0300

    All the necessary code was covered with autotests

--- src/test/kotlin/NoteServiceTest.kt
+++ src/test/kotlin/NoteServiceTest.kt
@@ -209,4 +209,333 @@ class NoteServiceTest {
 
         assertEquals(null, result?.id)
     }
+
+    @Test
+    fun addCommentToExistingNoteIfCommentsListIsEmpty() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+
+        val result = note.comments.size
+
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun addCommentToExistingNoteIfCommentsListIsNotEmpty() {
+        val service = NoteService()
+        val commentFirst = Comment(text = "FirstComment")
+        val commentSecond = Comment(text = "SecondComment")
+        val note = Note(
+            title = "SomeTitle",
+            text = "SomeText",
+            comments = mutableListOf()
+        )
+
+        service.add(note)
+        service.createComment(1, commentFirst)
+        service.createComment(1, commentSecond)
+
+        val result = commentSecond.id
+
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun addCommentToAbsentNote() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(2312, comment)
+
+        val result = note.comments.size
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun deleteCommentFromExistingNote() {
+        val service = NoteService()
+        val commentFirst = Comment(text = "FirstComment")
+        val commentSecond = Comment(text = "SecondComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, commentFirst)
+        service.createComment(1, commentSecond)
+        service.deleteComment(1, 1)
+
+        val result = commentFirst.isDeleted
+
+        assertTrue(result)
+    }
+
+    @Test
+    fun deleteCommentFromAbsentNote() {
+        val service = NoteService()
+        val commentFirst = Comment(text = "FirstComment")
+        val commentSecond = Comment(text = "SecondComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, commentFirst)
+        service.createComment(1, commentSecond)
+        service.deleteComment(2, 1)
+
+        val result = note.comments.size
+
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun deleteDeletedComment() {
+        val service = NoteService()
+        val commentFirst = Comment(text = "FirstComment", isDeleted = true)
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, commentFirst)
+        service.deleteComment(1, 1)
+
+        val resultFirst = note.comments.size
+        val resultSecond = commentFirst.isDeleted
+
+        assertEquals(1, resultFirst)
+        assertEquals(true, resultSecond)
+    }
+
+    @Test
+    fun restoreCommentFromExistingNote() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+        service.deleteComment(1, 1)
+        service.restoreComment(1,1)
+
+        val resultFirst = comment.isDeleted
+        val resultSecond = note.comments.size
+
+        assertFalse(resultFirst)
+        assertEquals(1, resultSecond)
+    }
+
+    @Test
+    fun restoreCommentFromAbsentNote() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+        service.deleteComment(1, 1)
+        service.restoreComment(2,1)
+
+        val resultFirst = comment.isDeleted
+        val resultSecond = note.comments.size
+
+        assertTrue(resultFirst)
+        assertEquals(1, resultSecond)
+    }
+
+    @Test
+    fun restoreCommentThatWasNotDeleted() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment", isDeleted = false)
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+        service.restoreComment(1,1)
+
+        val resultFirst = comment.isDeleted
+        val resultSecond = note.comments.size
+
+        assertFalse(resultFirst)
+        assertEquals(1, resultSecond)
+    }
+
+    @Test
+    fun restoreUnexistingComment() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment", isDeleted = false)
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+        service.restoreComment(1,2)
+
+        val result = note.comments.size
+
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun getCommentsForExistingNote() {
+        val service = NoteService()
+        val commentFirst = Comment(text = "FirstComment")
+        val commentSecond = Comment(text = "SecondComment")
+        val commentThird = Comment(text = "ThirdComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, commentFirst)
+        service.createComment(1, commentSecond)
+        service.createComment(1, commentThird)
+
+        val result = service.getComments(1)?.size
+
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun getCommentsForAbsentNote() {
+        val service = NoteService()
+        val commentFirst = Comment(text = "FirstComment")
+        val commentSecond = Comment(text = "SecondComment")
+        val commentThird = Comment(text = "ThirdComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, commentFirst)
+        service.createComment(1, commentSecond)
+        service.createComment(1, commentThird)
+
+        val result = service.getComments(2)?.size
+
+        assertEquals(null, result)
+    }
+
+    @Test
+    fun getOnlyNotDeletedCommentsForExistingNote() {
+        val service = NoteService()
+        val commentFirst = Comment(text = "FirstComment", isDeleted = true)
+        val commentSecond = Comment(text = "SecondComment")
+        val commentThird = Comment(text = "ThirdComment", isDeleted = true)
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, commentFirst)
+        service.createComment(1, commentSecond)
+        service.createComment(1, commentThird)
+
+        val result = service.getComments(1)?.size
+
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun editCommentForExistingNote() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+        service.editComment(1, 1, "NewText")
+
+        val result = comment.text
+
+        assertEquals("NewText", result)
+    }
+
+    @Test
+    fun editCommentForAbsentNote() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment")
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+        service.editComment(2, 1, "NewText")
+
+        val result = comment.text
+
+        assertEquals("SomeComment", result)
+    }
+
+    @Test
+    fun editDeletedComment() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment", isDeleted = true)
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+        service.editComment(1, 1, "NewText")
+
+        val result = comment.text
+
+        assertEquals("SomeComment", result)
+    }
+
+    @Test
+    fun editUnexistingComment() {
+        val service = NoteService()
+        val comment = Comment(text = "SomeComment", isDeleted = false)
+        val note = Note(
+            title = "FirstTitle",
+            text = "FirstText",
+            comments = mutableListOf()
+        )
+        service.add(note)
+        service.createComment(1, comment)
+        service.editComment(1, 2, "NewText")
+
+        val result = comment.text
+
+        assertEquals("SomeComment", result)
+    }
 }
